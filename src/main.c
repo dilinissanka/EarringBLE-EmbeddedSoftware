bfd0631fc9f9fd140c6efd71cb5ad42e74066722
dilinissanka <111609471+dilinissanka@users.noreply.github.com>
2023-11-28 16:02:12 -0800

    Update main.c - send accelerometer data

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <dk_buttons_and_leds.h>
 #include <zephyr/drivers/sensor.h>
 #include <stdlib.h>
+#include <zephyr/drivers/i2c.h>
 
 #include "remote.h"
 
@@ -16,11 +17,12 @@
 #define RUN_LED_BLINK_INTERVAL 20
 
 bool connected = false;
-int count = -1;
+int count = 0;
 int val = -1;
 static struct bt_conn *current_conn;
 
 struct device *lis2dw12 = DEVICE_DT_GET_ANY(st_lis2dw12);
+struct device *accel = DEVICE_DT_GET(DT_NODELABEL(i2c1));
 const struct device *const dev = DEVICE_DT_GET_ANY(maxim_max30101);
 
 /* Declarations */
@@ -106,25 +108,33 @@ void accel_updatev1()
 	sensor_sample_fetch(lis2dw12);
 	sensor_channel_get(lis2dw12, SENSOR_CHAN_ACCEL_XYZ, acc);
 	sensor_sample_fetch(dev);
-	sensor_channel_get(dev, SENSOR_CHAN_IR, &ir);
+	sensor_channel_get(dev, SENSOR_CHAN_GREEN, &ir);
 	set_accel_status((int8_t)acc[0].val1,(int8_t)acc[0].val2, (int8_t)acc[1].val1, (int8_t)acc[1].val2, (int8_t)acc[2].val1, (int8_t)acc[2].val2,(uint16_t)ir.val1 ,count);
 	if(count == 29) {
 		send_button_notification(current_conn);
 	}
 }
 
-void accel_updatev2()
+void ppg()
 {
 	uint8_t data[240] = {0};
 	for(int i = 0; i < 30; i++) {
 		//data[i] = 0xFF;
 		struct sensor_value acc[3];
 	 	struct sensor_value ir;
+		sensor_sample_fetch(lis2dw12);
+		sensor_channel_get(lis2dw12, SENSOR_CHAN_ACCEL_XYZ, acc);
 	 	sensor_sample_fetch(dev);
 	 	sensor_channel_get(dev, SENSOR_CHAN_IR, &ir);
 	 	uint16_t ppg = (uint16_t)ir.val1;
-	 	data[i*2] = (ppg >> 8) & 0xFF;
-	 	data[(i * 2) + 1] = ppg & 0xFF;
+	 	data[i*8] = (ppg >> 8) & 0xFF;
+	 	data[(i * 8) + 1] = ppg & 0xFF;
+		data[(i*8) + 2] = (uint8_t)(acc[0].val1 & 0xFF);
+		data[(i*8) + 3] = (uint8_t)(acc[0].val2 & 0xFF);
+		data[(i*8) + 4] = (uint8_t)(acc[1].val1 & 0xFF);
+		data[(i*8) + 5] = (uint8_t)(acc[1].val2 & 0xFF);
+		data[(i*8) + 6] = (uint8_t)(acc[2].val1 & 0xFF);
+		data[(i*8) + 7] = (uint8_t)(acc[2].val2 & 0xFF);
 	}
 	set_ppg(data);
 	send_button_notification(current_conn);
@@ -142,38 +152,38 @@ void main(void)
 {
 	int err;
 	int blink_status = 0;
+	/*printk("Hello World! %s\n", CONFIG_BOARD);
+	err = bluetooth_init(&bluetooth_callbacks, &remote_callbacks);
+	if (err) {
+		printk("bt_enable returned %d", err);
+	}*/
+
+	if(!device_is_ready(lis2dw12)) {
+		//printk("lis2dw12 device is not ready\n");
+		return;
+	}
+
+
+	uint8_t buf[2] = {0x2E, 0xD0};
+	int ret = i2c_write(accel, buf, 2, 0x19);
+	if(ret != 0) {
+		return;
+	}
+
 	printk("Hello World! %s\n", CONFIG_BOARD);
 	err = bluetooth_init(&bluetooth_callbacks, &remote_callbacks);
 	if (err) {
 		printk("bt_enable returned %d", err);
 	}
-	printk("Running...");
-	//k_work_init(&lis2dw12_work,accel_update);
-	//k_timer_init(&mytimer, mytimer_cb,NULL);
-	//k_timer_start(&mytimer, K_MSEC(20), K_MSEC(20));
 
-	if(!device_is_ready(lis2dw12)) {
-		printk("lis2dw12 device is not ready\n");
-		return;
-	}
+	// send data to register where i enable fifo
 
 	if (!device_is_ready(dev)) {
 		printf("max30101 device %s is not ready\n", dev->name);
 		return;
 	}
-
-    /*for(int i = 0; i < 10; i++){
-		k_sleep(K_MSEC(1000));
-	}*/
-
 	for (;;) {
-		accel_updatev2();
-		/*if(count == 29) {
-			count = -1;
-		}*/
-
-		//k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+		ppg();
 		k_sleep(K_MSEC(600));
 	}
-	//k_timer_stop(&mytimer);
 }
