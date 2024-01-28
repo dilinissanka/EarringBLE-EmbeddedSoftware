ccee9590c61150e6901987cbb7ab6835405f5399
dilinissanka <111609471+dilinissanka@users.noreply.github.com>
2024-01-28 14:27:33 -0800

    Update main.c - for pcb

--- src/main.c
+++ src/main.c
@@ -4,10 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
-#include <dk_buttons_and_leds.h>
+//#include <zephyr/kernel.h>
+//#include <dk_buttons_and_leds.h>
 #include <zephyr/drivers/sensor.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <zephyr/drivers/i2c.h>
 
 #include "remote.h"
@@ -51,17 +51,17 @@ struct bt_remote_service_cb remote_callbacks = {
 void on_connected(struct bt_conn *conn, uint8_t err)
 {
 	if(err) {
-		printk("connection err: %d", err);
+		//printk("connection err: %d", err);
 		return;
 	}
-	printk("Connected.");
+	//printk("Connected.");
 	current_conn = bt_conn_ref(conn);
 	request_mtu_exchange(conn);
 }
 
 void on_disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	printk("Disconnected (reason: %d)", reason);
+	//printk("Disconnected (reason: %d)", reason);
 	if(current_conn) {
 		bt_conn_unref(current_conn);
 		current_conn = NULL;
@@ -74,16 +74,16 @@ void on_le_data_len_updated(struct bt_conn *conn, struct bt_conn_le_data_len_inf
     uint16_t tx_time    = info->tx_max_time;
     uint16_t rx_len     = info->rx_max_len;
     uint16_t rx_time    = info->rx_max_time;
-    printk("Data length updated. Length %d/%d bytes, time %d/%d us", tx_len, rx_len, tx_time, rx_time);
+   // printk("Data length updated. Length %d/%d bytes, time %d/%d us", tx_len, rx_len, tx_time, rx_time);
 }
 
 void on_notif_changed(enum bt_button_notifications_enabled status)
 {
 	if (status == BT_BUTTON_NOTIFICATIONS_ENABLED) {
-		printk("Notifications enabled");
+		//printk("Notifications enabled");
 	}
 	else {
-		printk("Notificatons disabled");
+		//printk("Notificatons disabled");
 	} 
 }
 
@@ -93,7 +93,7 @@ void on_data_received(struct bt_conn *conn, const uint8_t *const data, uint16_t 
 	memcpy(temp_str, data, len);
 	temp_str[len] = 0x00;
 
-	printk("Received data on conn %p. Len: %d", (void *)conn, len);
+	//printk("Received data on conn %p. Len: %d", (void *)conn, len);
 }
 
 
@@ -125,24 +125,24 @@ void ppg()
 		struct sensor_value acc[3];
 	 	struct sensor_value ir;
 		struct sensor_value temp;
-		sensor_sample_fetch(lis2dw12);
-		sensor_channel_get(lis2dw12, SENSOR_CHAN_ACCEL_XYZ, acc);
+		//sensor_sample_fetch(lis2dw12);
+		//sensor_channel_get(lis2dw12, SENSOR_CHAN_ACCEL_XYZ, acc);
 	 	sensor_sample_fetch(dev);
-	 	sensor_channel_get(dev, SENSOR_CHAN_IR, &ir);
-		sensor_sample_fetch(hdc2010);
-		sensor_channel_get(hdc2010, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+	 	sensor_channel_get(dev, SENSOR_CHAN_GREEN, &ir);
+		//sensor_sample_fetch(hdc2010);
+		//sensor_channel_get(hdc2010, SENSOR_CHAN_AMBIENT_TEMP, &temp);
 	 	uint16_t ppg_data = (uint16_t)ir.val1;
-	 	data[i*10] = (ppg_data >> 8) & 0xFF;
+		data[i*10] = (ppg_data >> 8) & 0xFF;
 	 	data[(i*10) + 1] = ppg_data & 0xFF;
-		data[(i*10) + 2] = (uint8_t)(acc[0].val1 & 0xFF);
-		data[(i*10) + 3] = (uint8_t)(acc[0].val2 & 0xFF);
-		data[(i*10) + 4] = (uint8_t)(acc[1].val1 & 0xFF);
-		data[(i*10) + 5] = (uint8_t)(acc[1].val2 & 0xFF);
-		data[(i*10) + 6] = (uint8_t)(acc[2].val1 & 0xFF);
-		data[(i*10) + 7] = (uint8_t)(acc[2].val2 & 0xFF);
+		//data[(i*10) + 2] = (uint8_t)(acc[0].val1 & 0xFF);
+		//data[(i*10) + 3] = (uint8_t)(acc[0].val2 & 0xFF);
+		//data[(i*10) + 4] = (uint8_t)(acc[1].val1 & 0xFF);
+		//data[(i*10) + 5] = (uint8_t)(acc[1].val2 & 0xFF);
+		//data[(i*10) + 6] = (uint8_t)(acc[2].val1 & 0xFF);
+		//data[(i*10) + 7] = (uint8_t)(acc[2].val2 & 0xFF);
 
-		data[(i*10) + 8] = (uint8_t)(temp.val1 & 0xFF);
-		data[(i*10) + 9] = (uint8_t)(temp.val2 & 0xFF);
+		//data[(i*10) + 8] = (uint8_t)(temp.val1 & 0xFF);
+		//data[(i*10) + 9] = (uint8_t)(temp.val2 & 0xFF);
 
 	}
 	set_ppg(data);
@@ -167,14 +167,15 @@ void main(void)
 		printk("bt_enable returned %d", err);
 	}*/
 
-	if(!device_is_ready(lis2dw12)) {
+	/*if(!device_is_ready(lis2dw12)) {
 		//printk("lis2dw12 device is not ready\n");
 		return;
-	}
+	}*/
 
-	if (!device_is_ready(hdc2010)) {
+	
+	/*if (!device_is_ready(hdc2010)) {
 		return;
-	}
+	}*/
 
 
 	/*uint8_t buf[2] = {0x2E, 0xD0};
@@ -188,10 +189,10 @@ void main(void)
 		return;
 	}
 
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	//printk("Hello World! %s\n", CONFIG_BOARD);
 	err = bluetooth_init(&bluetooth_callbacks, &remote_callbacks);
 	if (err) {
-		printk("bt_enable returned %d", err);
+		//printk("bt_enable returned %d", err);
 	}
 
 	// send data to register where i enable fifo
